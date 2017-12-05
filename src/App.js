095c62cc4a623b665a870401c9557d9c1f94e6e8
Tom Milligan <tom@tommilligan.net>
2017-12-05 19:00:11 +0000

    Working semantic ui single page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// @flow
+
+import React from 'react';
+import { Container, Header, Segment } from 'semantic-ui-react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = props => (
+  <div className="App">
+    <Segment
+    inverted
+    id='header'
+    style={{ padding: 8, backgroundColor: '#5a47a5', borderRadius: 0 }}
+    >
+    <Header
+    inverted
+    as='h3'
+    textAlign='center'
+    style={{ margin: 0 }}
+    >
+      spam
+    </Header>
+    </Segment>
+    <Container>
+      eggs
+    </Container>
+  </div>
+)
 
 export default App;
