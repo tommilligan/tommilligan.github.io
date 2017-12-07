2f9758f84a95c4954bbdd7cc798abc72a76aa300
Tom Milligan <tom@tommilligan.net>
2017-12-07 22:19:52 +0000

    Added tech banner with iterable all logos

--- src/App.js
+++ src/App.js
@@ -5,29 +5,53 @@ import { Container, Header, Segment } from "semantic-ui-react";
 import "./App.css";
 import Logo, {LogoEnum} from "./components/logo";
 
-const App = (props: any) => (
-  <div className="App">
-    <Segment
-      inverted
-      id="header"
-      style={{ padding: 8, backgroundColor: "#5a47a5", borderRadius: 0 }}
-    >
-      <Header
+const App = (props: any) => {
+  const logoArray = Object.keys(LogoEnum).map(logoKey => {
+    return (
+      <div key={logoKey} style={{ margin: "0 5px" }}>
+        <Logo name={LogoEnum[logoKey]} />
+      </div>
+    );
+  });
+  return (
+    <div className="App">
+      <Segment
         inverted
-        as="h3"
-        textAlign="center"
-        style={{ margin: 0 }}
+        id="header"
+        style={{ padding: 8, backgroundColor: "#5a47a5", borderRadius: 0 }}
       >
-        spam
-      </Header>
-    </Segment>
-    <Container>
-      eggs
-      <Logo name={LogoEnum.PYTHON} />
-      <Logo name={LogoEnum.HEROKU} />
-      <Logo name={LogoEnum.ELASTICSEARCH} />
-    </Container>
-  </div>
-);
+        <Header
+          inverted
+          as="h3"
+          textAlign="center"
+          style={{ margin: 0 }}
+        >
+          spam
+        </Header>
+      </Segment>
+      <Container>
+        eels
+      </Container>
+      <Segment
+        inverted
+        style={{ padding: 8, backgroundColor: "#302659", borderRadius: 0 }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            alignItems: "center",
+            justifyContent: "center"
+          }}>
+          {logoArray}
+        </div>
+      </Segment>
+      <Container>
+        <Logo name={LogoEnum.PYTHON} />
+      </Container>
+    </div>
+  );
+};
 
 export default App;
